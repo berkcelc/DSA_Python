fbed9f20dc74e212151affb5c990482d6353ab9b
Purushotam Adarshi <purushotam.adarshi@gmail.com>
2023-06-30 17:11:03 +0530

    Lists

---
+++ 4. Lists/lists.py
@@ -0,0 +1,36 @@
+a = [1,3,5]
+b = [2,4,6]
+
+# concatenate the lists
+c = a + b
+
+# repeat the element n times
+a * 4
+
+a = 'span'
+
+b = list(a)
+
+b
+
+'gaurav'.capitalize()
+
+
+def ff(value, values):
+    v = 1
+    values[0] = 44
+
+t = 3
+v = [1,2,3]
+ff(t,v)
+
+t,v[0]
+
+
+
+a=[1,2,3,4,5,6,7,8,9]
+a[::2]=10,20,30,40,50,60
+print(a)
+
+a=[1,2,3,4,5]
+print(a[3:0:-1])
