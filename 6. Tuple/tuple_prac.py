3a2045d2f167976da1e69ac68c8b6d3fd4284bd7
Purushotam Adarshi <purushotam.adarshi@gmail.com>
2023-07-04 14:53:42 +0530

    Tuples final

--- 6. Tuple/tuple_prac.py
+++ 6. Tuple/tuple_prac.py
@@ -23,3 +23,43 @@ min(t2) # returns the minimum
 
 # Time complexity of creating a tuple is  O(1)
 
+
+# element wise addition
+
+t1 = 1,2,3
+t2 = 4,5,6
+
+
+t1 + t2
+
+
+# there are 2 ways to do element wise operation for given two tuple
+
+tuple(a + b for (a, b) in zip(t1, t2)) # we need to write the tuple statement
+tuple(map(sum, zip(t1, t2)))
+
+('Hello', 'world')
+
+
+def concatenate_strings(input_tuple):
+    text = input_tuple[0]
+    print(text)
+    for i in range(1, len(input_tuple)):
+        print(input_tuple[i])
+        text = text + ' ' + input_tuple[i]
+    return text
+    
+
+concatenate_strings(('Hello', 'world'))
+
+# a very short version
+' '.join(('Hello', 'world'))
+
+len(t1)
+
+def get_diagonal(input_tuple):
+    return tuple(input_tuple[i][i] for i in range(len(input_tuple)))
+
+# to get the common elements between two tuples
+# tuple(set(tuple1) & set(tuple2))
+
