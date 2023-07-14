2e0c3a30947198c124cf5bfe5adc8c8fa02e8fca
Purushotam Adarshi <purushotam.adarshi@gmail.com>
2023-07-14 10:15:51 +0530

    Creation of csll

--- 9. Circular Linked List/cll.py
+++ 9. Circular Linked List/cll.py
@@ -8,9 +8,28 @@ class CircularSinglyLinkedList:
         self.head = None
         self.tail = None
     def __iter__(self):
+
+        # This function is for printing the values of the linked list
+
         node = self.head
         while node:
             yield node
             if node.next == self.head:
                 break
             node = node.next
+
+
+    # creating a node
+    def create(self, value):
+        node = Node(value)
+        node.next = node    
+        self.head = node
+        self.tail = node
+        return "The CSLL has been created"
+    
+circulrSLL = CircularSinglyLinkedList()
+circulrSLL.create(1)
+
+
+# for this reason we used the Iter tool
+[node.value for node in circulrSLL]
