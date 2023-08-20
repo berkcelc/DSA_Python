d445c3c71eb0b635a4126f605538706dfc52e2d7
Gaurav <purushotam.adarshi@gmail.com>
2023-08-20 14:32:36 +0530

    circular queue

--- 13. Queue/circular_queue.py
+++ 13. Queue/circular_queue.py
@@ -24,13 +24,56 @@ class Queue:
             return False
 
     def enqueue(self,value):
-        if self.top + 1 == self.maxsize:
-            self.top = 0
+        if self.isFull():
+            return "The queue is Full"
         else:
-            self.top += 1
-            if self.start == -1:
+            if self.top + 1 == self.maxsize:
+                self.top = 0
+            else:
+                self.top += 1
+                if self.start == -1:
+                    self.start = 0
+            self.items[self.top] = value
+            return "The element is inserted in the queue"
+        
+    def dequeue(self):
+        if self.isEmpty():
+            return "The queue is empty"
+        else:
+            val = self.items[self.start]
+            self.items[self.start] = None
+            if self.start == self.top:
+                self.start = self.top = -1
+            elif self.start + 1 == self.maxsize:
                 self.start = 0
-        self.items[self.top] = value
-        return "The element is inserted in the queue"
+            else:
+                self.start += 1
+            return val
+        
+    def peek(self):
+        if self.isEmpty():
+            return "The queue is empty"
+        else:
+            return self.items[self.start]
+        
+    def delete(self):
+        self.items = self.maxsize * [None]
+        self.start = self.top = -1
+
+        
+
+q = Queue(3)
     
-    
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+print(q)
+
+q.enqueue(1)
+q.dequeue()
+
+q.isFull()
+ 
+q.peek()
+
