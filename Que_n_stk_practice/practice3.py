ffdabc97b93f1651083199559665a73a59f8a7e6
Gaurav <gauravkumarquant@gmail.com>
2023-08-25 15:26:27 +0530

    multistack

--- Que_n_stk_practice/practice3.py
+++ Que_n_stk_practice/practice3.py
@@ -11,6 +11,13 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+    def __iter__(self):
+        c_node = self.head
+        while c_node:
+            yield c_node
+            c_node = c_node.next
+
+
 
 class MasterStack:
     def __init__(self, size):
@@ -18,6 +25,11 @@ class MasterStack:
         self.n_substack = 0
         self.stack_size = size
         self.count_element = 0
+
+    def __str__(self):
+        vals = [str(x.value) for x in self.linkedlist]
+        return vals.reverse()
+
 
     def push(self, value):
         new_node = Node(value)
@@ -30,4 +42,43 @@ class MasterStack:
             new_node.next = self.linkedlist.head
             self.linkedlist.head = new_node
 
+    def pop(self):
+        if self.linkedlist.head == None:
+            return "the stack is empty"
+        else:
+            self.count_element -= 1
+            if self.count_element % self.stack_size == 0:
+                self.n_substack = self.count_element // self.stack_size
+            else:
+                self.n_substack = (self.count_element // self.stack_size) + 1
+            val = self.linkedlist.head
+            self.linkedlist.head = self.linkedlist.head.next
+            return val.value
+        
+    def popat(self, index):
+        if index > self.n_substack:
+            return "The address does not reside in the location"
+        else:
+            position = ((index-1) * self.stack_size) + 1
+            print(position)
+            # we will move to the position 
+            pointer = self.linkedlist.head
+            for _ in range(position - 2):
+                pointer = pointer.next
+            val = pointer.next
+            
+            pointer = pointer.next.next
+            return val.value
 
+
+ms = MasterStack(3)
+ms.push(1)
+ms.push(2)
+ms.push(3)
+ms.push(4)
+
+ms.pop()
+
+ms.n_substack
+
+ms.popat(2)
