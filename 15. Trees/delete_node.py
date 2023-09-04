b0cd3952e3cebe227797d6ab57f61238422135d4
Gaurav <gauravkumarquant@gmail.com>
2023-09-04 11:50:31 +0530

    node_deletion complete

--- 15. Trees/delete_node.py
+++ 15. Trees/delete_node.py
@@ -45,12 +45,52 @@ def get_deepest_node(rootnode):
 get_deepest_node(newbt)
 
 
-def delete_deepest_node(rootnode, dnode):
+def delete_deepest_node(rootnode):
+    dnode = get_deepest_node(newbt)
     if not rootnode:
         return
     else:
         _temp = []
         _temp.append(rootnode)
-        
+        while len(_temp) > 0:
+            _ret = _temp.pop(0)
+            if _ret.data == dnode:
+                _ret = None
+            if _ret.rightchild:
+                if _ret.rightchild.data == dnode:
+                    _ret.rightchild = None
+                    return
+                else:
+                    _temp.append(_ret.rightchild)
+                
+            if _ret.leftchild:
+                if _ret.leftchild.data == dnode:
+                    _ret.leftchild = None
+                    return
+                else:
+                    _temp.append(_ret.leftchild)
+                
 
 
+def delete_node(rootnode, node):
+    if not rootnode:
+        return 
+    else:
+        _temp = []
+        _temp.append(rootnode)
+
+        while len(_temp) > 0:
+            _ret = _temp.pop(0)
+            if _ret.data == node:
+                dnode = get_deepest_node(rootnode)
+                print(dnode)
+                delete_deepest_node(rootnode=rootnode)
+                _ret.data = dnode
+                return "The link has been successfully deleted"
+            if (_ret.leftchild) is not None:
+                _temp.append(_ret.leftchild)
+            if _ret.rightchild is not None:
+                _temp.append(_ret.rightchild)
+        return "Failed to delete"
+
+delete_node(newbt, 'Hot')
