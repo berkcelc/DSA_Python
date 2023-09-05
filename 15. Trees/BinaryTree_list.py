bd1cee2a844e349e72ab88b0003e6d6ae543198e
Gaurav <gauravkumarquant@gmail.com>
2023-09-05 11:04:56 +0530

    traversals

--- 15. Trees/BinaryTree_list.py
+++ 15. Trees/BinaryTree_list.py
@@ -7,16 +7,39 @@ class BinayTree:
     def insertnode(self, value):
         if self.lastusedindex + 1 == self.maxsize:
             return "The binary tree is full"
-        else:
-            self.customlist[self.lastusedindex + 1] == value
-            self.lastusedindex += 1
-            return "The value has been successfully inserted"
-        
+    
+        print(value, self.lastusedindex)
+        self.customlist[self.lastusedindex + 1] = value
+        self.lastusedindex += 1
+        return "The value has been successfully inserted"
+    
     def searchnode(self, value):
-        for i in range(self.customlist):
+        for i in range(self.maxsize):
             if self.customlist[i] == value:
                 return "Success"
         return "Not Found"
+    
+    def preordertraversal(self, index):
+        if index > self.lastusedindex:
+            return
+        print(self.customlist[index])
+        self.preordertraversal(index*2)
+        self.preordertraversal(index*2 + 1)
+
+# there is some issue with preorder traversal, debug it 
+
 
 newbt = BinayTree(10)
-        
+newbt.insertnode('Drinks')     
+newbt.insertnode('Hot')     
+newbt.insertnode('Cold')     
+newbt.insertnode('Tea')     
+newbt.insertnode('Coffee')     
+newbt.insertnode('Cola')     
+newbt.insertnode('Fanta')
+
+newbt.customlist
+
+newbt.searchnode('Fanta')
+
+newbt.preordertraversal(0)
